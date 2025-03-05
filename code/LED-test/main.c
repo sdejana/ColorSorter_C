18205bc21d0332132ec2d131c833d796bd534811
Dejana Smiljanic <dejana.smiljanic@student.etf.unibl.org>
2025-03-05 19:58:47 +0100

    Issue #17: Dodajem testni kod za LED modul (radi provjereno).

--- code/LED-test/main.c
+++ code/LED-test/main.c
@@ -1,62 +1,15 @@
-#include <xc.h>
+void main() {
 
-/*
- * @author Dejana
- */
-/*
- * PIN 15 mikrokontrolera se koristi kao signalni.
- * PIN 15 = RB7.
- * Funkcija: treba da signalizuje pocetak rada sklopa.
- * Period: T = 250ms zasad.
- * Pin na plocici: desni header 6, odozgo.
- */
+    AD1PCFG = 0XFFFF;
 
-// Configuration Bits
-#pragma config FNOSC = FRC   // 8 MHz Fosc
-// #pragma config POSCMD = NONE    // Primary Oscillator disabled
-// #pragma config FWDTEN = OFF     // Watchdog Timer disabled
-// #pragma config JTAGEN = OFF     // JTAG disabled
+    // konfiguracija I/O portova
+   // TRISA.F0 = 1;                 // RA0 input pin
+    TRISB = 0;                    // RB output pins
 
-#define LED_PIN LATBbits.LATB7
+    LATB = 0;                     // All RBn pins are set to zero
 
-
-void __attribute__((__interrupt__, auto_psv)) _T1Interrupt(void) 
-{
-    LED_PIN ^= 1;  
-    IFS0bits.T1IF = 0; /* timer interrupt flag clear*/
-}
-
-void Timer1_Init(void) 
-{
-    T1CON = 0;            /* Reset Timer1 settings */
-    T1CONbits.TCKPS = 3;  /* 1:256 prescaler */
-    PR1 = 3906;  /* Timer period od ~250ms sa Fosc/2, 4MHz */
-    
-    /*
-     * PR1 = (T*Fcy)/prescaler
-     */
-    
-    TMR1 = 0;             /* Reset timer count */
-    IEC0bits.T1IE = 1;    /* Enable Timer1 interrupt */
-    IFS0bits.T1IF = 0;    /* Clear interrupt flag */
-    T1CONbits.TON = 1;    /* Start Timer1 */
-}
-
-void GPIO_Init(void) 
-{
-    AD1PCFGbits.PCFG15 = 1; /* podesavam samo pin koji koristim na D*/
-    TRISBbits.TRISB7 = 0; 
-    LED_PIN = 0;          
-}
-
-int main(void) 
-{
-    GPIO_Init();  
-    Timer1_Init(); 
-
-    while(1) 
-    {
+    while(1){
+        LATB = ~LATB;
+        Delay_ms(1000);
     }
-    
-    return 0;
 }
