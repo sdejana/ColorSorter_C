f8ab4a605610ee8200149a6900ec78cc2bd1492f
Jelena Radakovic <jelena.radakovic@student.etf.unibl.org>
2025-04-17 13:56:33 +0200

    Issue #26: Dodajem prvu verziju itegracije Potrebno itegrisati Color senzor

---
+++ code/temp/UART_test_code.X/LED.c
@@ -0,0 +1,11 @@
+/*
+ * File:   LED.c
+ * Author: 23dej
+ *
+ * Created on April 10, 2025, 3:44 PM
+ */
+
+
+#include "xc.h"
+#include "LED.h"
+
---
+++ code/temp/UART_test_code.X/build/default/production/VEML3328.o.d
@@ -0,0 +1,10 @@
+build/default/production/VEML3328.o: VEML3328.c VEML3328.h system.h \
+ pin_manager.h interrupt_manager.h
+
+VEML3328.h:
+
+system.h:
+
+pin_manager.h:
+
+interrupt_manager.h:
