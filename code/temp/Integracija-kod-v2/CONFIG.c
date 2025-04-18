55257d926a9f7d35135de7e793348a847b75b9e4
Dejana Smiljanic <dejana.smiljanic@student.etf.unibl.org>
2025-04-18 11:17:12 +0200

    Issue #25: Dodajem projekat sa kodom koji sadrzi integrisane sve komponente. Potrebno je jos izmijeniti funkciju stopAll(void), te eventualno smanjiti vrijeme cekanja izmedju iteracija u programu.

---
+++ code/temp/Integracija-kod-v2/CONFIG.c
@@ -0,0 +1,90 @@
+/*
+ * File:   CONFIG.c
+ * Author: 23dej
+ *
+ * Created on April 2, 2025, 6:12 PM
+ */
+
+
+#include "xc.h"
+#include "CONFIG.h"
+#include "VEML3328.h"
+#include "WIFI.h"
+#include "SERVO.h"
+
+
+void configureOscillator()
+{
+    CLKDIV = 0;  // Set frequency divider to zero
+    __builtin_write_OSCCONH(0x01);  // Set FRC with PLL 
+    __builtin_write_OSCCONL(OSCCON | 0x01);  // Confirm change 
+    while (OSCCONbits.OSWEN);  // Wait until changes are done
+}
+
+void configurePPS()
+{
+    __builtin_write_OSCCONL(OSCCON & ~(1 << 6));  // PPS unlock
+
+    RPINR18bits.U1RXR = 14; // 14 RX connected to TX of ESP8266EX
+    RPOR5bits.RP10R = 3;    // 9 TX connected to RX of ESP8266EX
+    RPOR2bits.RP5R = 18;        // Map OC1 to RB5
+    RPOR5bits.RP11R = 0x0007;    //RB11->SPI1:SDO1
+    
+    __builtin_write_OSCCONL(OSCCON | (1 << 6));  // PPS lock
+}
+
+
+void configureIO() 
+{
+    AD1PCFG = 0xFFFF; 
+    TRISB = 0x0000;
+    TRISBbits.TRISB14 = 1; // Set RX pin as input pin
+    TRISBbits.TRISB10 = 0; // Set TX pin as output pin 
+    
+    TRISAbits.TRISA4 = 0; // Set enable pin as output pin
+    LATAbits.LATA4 = 1; // Set enable pin to high voltage
+    
+    TRISBbits.TRISB6 = 0; // set LED as output
+    TRISBbits.TRISB7 = 1; // set BUTTON as input
+    
+    LATBbits.LATB6 = 0;    
+    
+    INTCON2bits.INT0EP = 1; // falling edge
+    IEC0bits.INT0IE = 1; // interrupt enable 
+    IFS0bits.INT0IF = 0; // interrupt flag clear
+    IPC0bits.INT0IP = 7; // highest priority
+    
+    
+}
+
+void stopAll()
+{
+    flag = 1;
+    moveMiddle();
+    __delay_ms(200);
+    sendData("S\r\n");
+    LATBbits.LATB6 = 0;
+}
+
+void __attribute__((interrupt, auto_psv)) _INT0Interrupt(void) 
+{
+    IEC0bits.INT0IE = 0;
+    flag = ~flag;
+    if(flag)
+    {
+        stopAll();        
+    }    
+    IEC0bits.INT0IE = 1;
+    IFS0bits.INT0IF = 0;  // Clear INT0 flag
+}
+
+
+void configureAllComponents()
+{
+    configureOscillator();    
+    configureIO();
+    configurePPS();
+    colorClickConfiguration();
+    outputCompareConfig();
+    __delay_ms(1000);
+}
---
+++ code/temp/Integracija-kod-v2/build/default/production/BUTTON.o.d
@@ -0,0 +1,18 @@
+build/default/production/BUTTON.o: BUTTON.c BUTTON.h WIFI.h CONFIG.h \
+ VEML3328.h system.h pin_manager.h interrupt_manager.h SERVO.h
+
+BUTTON.h:
+
+WIFI.h:
+
+CONFIG.h:
+
+VEML3328.h:
+
+system.h:
+
+pin_manager.h:
+
+interrupt_manager.h:
+
+SERVO.h:
